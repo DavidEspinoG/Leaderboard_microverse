56b5dbeb08593226077b5bda90955a8059179f2e
David Espino <davidespinog@gmail.com>
2023-03-06 17:22:53 -0600

    Add eventListener to window

--- src/index.js
+++ src/index.js
@@ -3,19 +3,27 @@ import newScore from './modules/newScore.js';
 import generateHtml from './modules/generateHtml.js';
 import getScores from './modules/getScores.js';
 
-const form = document.getElementById('newScore.js');
-const scoreContainer = document.getElementById('scores__container.js');
-const refreshButton = document.getElementById('refresh-button.js');
+const form = document.getElementById('newScore');
+const scoreContainer = document.getElementById('scores__container');
+const refreshButton = document.getElementById('refresh-button');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = form.elements.name.value;
-  const score = parseInt(form.elements.score.value, 10);
+  const score = parseInt(form.elements.score.value);
   newScore(name, score);
 });
 
 refreshButton.addEventListener('click', () => {
   scoreContainer.innerHTML = '';
+  getScores().then((data) => {
+    data.forEach((element) => {
+      scoreContainer.appendChild(generateHtml(element));
+    });
+  });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
   getScores().then((data) => {
     data.forEach((element) => {
       scoreContainer.appendChild(generateHtml(element));
