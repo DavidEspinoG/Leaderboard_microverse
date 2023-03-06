57fde2adcdfa5382384351ca1ee525300bd09e66
David Espino G <davidespinog@gmail.com>
2023-03-06 13:28:09 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  module: {
+    rules: [
+      {
+        test: /\.css|scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
